f3d6d641514aa370ba8f9970cec6a47a04cb74a4
中尾圭吾 <knakao@nakaokeigonoiMac.local>
2020-06-08 23:36:19 +0900

    フロント実装

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-const app = new Vue({
-  el: '#app',
+let app = new Vue({
+  el: "#app",
   data: {
-    n: 0,  //入力値
-    i: 1,  //初期値
+    n: 0, //入力値
+    i: 1, //初期値
     a: 0,
     b: 0,
     aSqrt: 0,
-    bSqrt: 0
+    bSqrt: 0,
+    dif: 0,
   },
-  methods:{
-    sqrt:function(){
-      for( this.a = this.i ; this.a <= this.n ; this.a+=2 ){
+  methods: {
+    sqrt: function () {
+      this.aSqrt = 0;
+      this.bSqrt = 0;
+      this.i = 0;
+      for (this.a = this.i; this.a <= this.n; this.a += 2) {
         this.aSqrt += Math.sqrt(this.a);
       }
-      for( this.b = this.i + 1 ; this.b <= this.n ; this.b+=2 ){
+      for (this.b = this.i + 1; this.b <= this.n; this.b += 2) {
         this.bSqrt += Math.sqrt(this.b);
       }
-    }
-  }
-})
+      this.dif = this.aSqrt - this.bSqrt;
+    },
+    enter: function () {
+      if (window.event.keyCode == 13) {
+        this.sqrt();
+      }
+    },
+  },
+});
